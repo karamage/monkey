1aad431af4b608e07ca7d320ce3afd6d1b73e300
karamage <karamage.umauma@gmail.com>
2024-12-06 17:36:43 +0900

    add token types and constants for lexer

--- token/token.go
+++ token/token.go
@@ -1,1 +1,34 @@
 package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 12345
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
